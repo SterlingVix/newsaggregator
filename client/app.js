cdf7bb7bfbdd1d7768c6d6e3c1f9bae3a3998119
SterlingVix <amelocik@gmail.com>
2014-09-11 17:24:05 -0700

    Angular routing working. Time for styling and content.

--- client/app.js
+++ client/app.js
@@ -1,28 +1,12 @@
 angular.module('newsaggregator', [
-  'posts',
-  'ngRoute',
-  'ui.router'
+  'ui.router',
+  'newsaggregator.viewparent',
+  'viewparent.viewheader',
+  'viewparent.viewfeed',
+  'viewparent.viewiframe'
+  // 'ngRoute'
 ])
-// angular.module('app', [
-//   'viewparent',
-//   'posts',
-//   'ngRoute',
-//   'ui.router'
-// ])
-// .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-//   $stateProvider
-//     .state('home', {
-//       url: '/',
-//       // templateUrl: 'posts/posts.html',
-//       // controller: 'PostsController'
-//       templateUrl: 'viewparent/viewparent',
-//       controller: 'ParentController'
-//     });
-
-//   $urlRouterProvider.otherwise('/');
-    
-// }])
-.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider, $httpProvider) {
   $stateProvider
     .state('home', {
       url: '/',
@@ -37,32 +21,18 @@ angular.module('newsaggregator', [
   // [interceptors](https://github.com/angular/angular.js/blob/master/src/ng/http.js#L337)
   // $httpProvider.interceptors.push('AttatchTokens');
   
-})
+}]);
 
 
-.factory('GetPosts', function ($http, $q) {
-  var gotten = function() {
-    var deferred = $q.defer();
-    $http.get('http://127.0.0.1:8080/api/content').success(function(res) {
-      deferred.resolve(res);
-    });
-    return deferred.promise;
-  }
-  return {
-    gotten: gotten
-  };
-});
-
-// .config(function($httpProvider) {
-//   // Enables cross domain calls
-//   $httpProvider.defaults.useXDomain = true;
-//   // Removes the header used to identify ajax calls that would prevent CORS from working
-//   delete $httpProvider.defaults.headers.common['X-Requested-With'];
+// .factory('GetPosts', function ($http, $q) {
+//   var gotten = function() {
+//     var deferred = $q.defer();
+//     $http.get('http://127.0.0.1:8080/api/content').success(function(res) {
+//       deferred.resolve(res);
+//     });
+//     return deferred.promise;
+//   }
+//   return {
+//     gotten: gotten
+//   };
 // });
-// // .config(['$httpProvider', function($httpProvider) {
-//   $httpProvider.defaults.useXDomain = true;
-//   $httpProvider.defaults.withCredentials = true;
-//   delete $httpProvider.defaults.headers.common["X-Requested-With"];
-//   $httpProvider.defaults.headers.common["Accept"] = "application/json";
-//   $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-// }]);
