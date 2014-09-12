4b3cf475b85d59b98f3066eea2461ee4022b7c96
SterlingVix <amelocik@gmail.com>
2014-09-12 10:52:07 -0700

    Experimented with post controllers. These changes will probably be discarded soon.

--- client/viewfeed/viewpost/posts.controller.js
+++ client/viewfeed/viewpost/posts.controller.js
@@ -1,16 +1,16 @@
-// angular.module('posts', [
-//   'posts.directive',
-//   'getHTML'
-// //   ])
+angular.module('posts', [
+  'posts.directive',
+  'getHTML'
+//   ])
 
-//   .controller('PostsController', function($scope, GetPosts) {
-//     $scope.post=[];
-//     var getPosts = function() {
-//       GetPosts.gotten().then(function(res) {
-//         angular.forEach(res, function(value, key) {
-//           $scope.post.push(value);
-//         })
-//       })
-//     };
-//   getPosts();
-// });
+  .controller('PostsController', function($scope, GetPosts) {
+    $scope.post=[];
+    var getPosts = function() {
+      GetPosts.gotten().then(function(res) {
+        angular.forEach(res, function(value, key) {
+          $scope.post.push(value);
+        })
+      })
+    };
+  getPosts();
+});
